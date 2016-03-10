00880e35a5de37a08d4bd4deaa6ee922837fe33d
Mohammed sha <shakarim22w@gmail.com>
2016-03-10 12:08:51 +0530

    Adds Example reminders

--- app/src/main/java/com/example/sha/reminders/RemindersActivity.java
+++ app/src/main/java/com/example/sha/reminders/RemindersActivity.java
@@ -1,5 +1,6 @@
 package com.example.sha.reminders;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,12 +12,54 @@ import android.widget.ListView;
 public class RemindersActivity extends AppCompatActivity {
 
     private ListView lstListView;
+    private RemindersDbAdapter mDbAdapter;
+    private RemindersSimpleCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
         lstListView = (ListView)findViewById(R.id.lvReminders);
+        lstListView.setDivider(null);
+        mDbAdapter = new RemindersDbAdapter(this);
+        mDbAdapter.dbOpen();
+
+        if (savedInstanceState == null) {
+        //Clear all data
+            mDbAdapter.deleteAllReminders();
+            //Add some data
+            insertSomeReminders();
+        }
+
+        Cursor cur = mDbAdapter.fetchAllReminders();
+        //from columns defined in the db
+        String[] from = new String[]{
+                RemindersDbAdapter.COL_CONTENT
+        };
+
+        //to the ids of views in the layout
+        int[] to = new int[]{
+                R.id.row_text
+        };
+
+        mCursorAdapter =new RemindersSimpleCursorAdapter(
+                //context
+                RemindersActivity.this,
+                //the layout of the row
+                R.layout.reminders_row,
+                //cursor
+                cur,
+                //from columns defined in the db
+                from,
+                //to the ids of views in the layout                .
+                to,
+                //flag - not used
+                0
+        );
+        // the cursorAdapter (controller) is now updating the listView (view)
+        // with data from the db (model)
+        lstListView.setAdapter(mCursorAdapter);
+
         ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(
                 this,
                 R.layout.reminders_row,
@@ -25,6 +68,24 @@ public class RemindersActivity extends AppCompatActivity {
 
         );
         lstListView.setAdapter(arrayAdapter);
+    }
+
+    private void insertSomeReminders() {
+        mDbAdapter.createReminder("Buy Learn Android Studio", true);
+        mDbAdapter.createReminder("Send Dad birthday gift", false);
+        mDbAdapter.createReminder("Dinner at the Gage on Friday", false);
+        mDbAdapter.createReminder("String squash racket", false);
+        mDbAdapter.createReminder("Shovel and salt walkways", false);
+        mDbAdapter.createReminder("Prepare Advanced Android syllabus", true);
+        mDbAdapter.createReminder("Buy new office chair", false);
+        mDbAdapter.createReminder("Call Auto-body shop for quote", false);
+        mDbAdapter.createReminder("Renew membership to club", false);
+        mDbAdapter.createReminder("Buy new Galaxy Android phone", true);
+        mDbAdapter.createReminder("Sell old Android phone - auction", false);
+        mDbAdapter.createReminder("Buy new paddles for kayaks", false);
+        mDbAdapter.createReminder("Call accountant about tax returns", false);
+        mDbAdapter.createReminder("Buy 300,000 shares of Google", false);
+        mDbAdapter.createReminder("Call the Dalai Lama back", true);
     }
 
     @Override
